f7b91279f426e40087f517e07ed0aad2e4aed05d
Tala-Hodeib <talahodeib@gmail.com>
2020-10-22 13:15:39 +0300

    step3

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const app = express();
 const PORT = 3001;
+
+var today = new Date();
+var time = today.getHours() + ":" + today.getSeconds();
+
 app.get('/', (req, res) => {
   res.send('ok');
 });
+
+
+app.get('/test', function (req, res) {
+    res.send('{status:200, message:"ok"}')
+})
+
+app.get('/time', function (req, res) {
+    res.send({status:200, message:time})
+})
+
+
 app.listen(PORT, () => {
-  console.log(`Server running at: http://localhost:${PORT}/`);
+    console.log(`Server running at: http://localhost:${PORT}/`);
 });
+  
