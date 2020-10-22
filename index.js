cd2ad865bba6540c6c161a397ccb2e284832b1bd
Tala-Hodeib <talahodeib@gmail.com>
2020-10-22 15:03:55 +0300

    step4

--- index.js
+++ index.js
@@ -18,6 +18,24 @@ app.get('/time', function (req, res) {
     res.send({status:200, message:time})
 })
 
+app.get('/hello/:id', function (req, res) {
+    res.send({status:200,message:"Hello," + req.params.id})
+})
+
+app.get('/hello', function (req, res) {
+    res.send({status:200,message:"Hello"})
+})
+
+app.get('/search', function (req, res) {
+    const {s} = req.query;
+    if(!s){
+        res.send({status:500, error:true, message:"you have to provide a search"})
+
+    }
+    else{
+    res.send({status:200, message:"ok", data:s})}
+})
+
 
 app.listen(PORT, () => {
     console.log(`Server running at: http://localhost:${PORT}/`);
