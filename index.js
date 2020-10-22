81a3de3bcff247ebf87ab01da2c612acdaf63960
Tala-Hodeib <talahodeib@gmail.com>
2020-10-22 15:17:50 +0300

    step5

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 const PORT = 3001;
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
 
 var today = new Date();
 var time = today.getHours() + ":" + today.getSeconds();
@@ -35,6 +41,24 @@ app.get('/search', function (req, res) {
     else{
     res.send({status:200, message:"ok", data:s})}
 })
+
+app.get('/movies/create', function (req, res){
+    res.send('create movies')
+})
+
+app.get('/movies/read', function (req, res){
+    res.send({status:200, data: movies })
+})
+
+app.get('/movies/update' , function (req, res){
+    res.send('update movies')
+})
+
+app.get('/movies/delete', function (req, res){
+    res.send('delete movies')
+})
+
+
 
 
 app.listen(PORT, () => {
